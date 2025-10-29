b28ca2744bbfeb37967956f053535ae01c825b20
Rimjhim11 <rimjhimprakash11@gmail.com>
2025-10-29 10:23:27 +0530

    React+vite Component

---
+++ ReactWork/reactviteapp/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Profile from './component/Profile'
+
+function App() {
+ 
+
+  return (
+    <div className='container'>
+      <h2>Welcome to ReactVite.</h2>
+        <Profile />
+
+  
+    </div>
+  )
+}
+
+export default App
---
+++ ReactWork/reactviteapp/src/component/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './profile.css'
+
+function Profile() {
+  return (
+    <div className='parent'>
+        <h2>Profile Page</h2>
+        <h3>Name: Rimjhim Prakash</h3>
+        <h3>Branch:CS</h3>
+    </div>
+  )
+}
+
+export default Profile
